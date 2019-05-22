bc4c37c12b49d752d769e84d50e9378b0661ea02
Pamodya Jayangani <pamodyajayangani@Pamodyas-Mac.local>
2019-05-21 20:38:20 -0700

    Initial Commit

---
+++ PMTool/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  PMTool
+//
+//  Created by Pamodya Jayangani on 5/21/19.
+//  Copyright © 2019 Pamodya Jayangani. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
