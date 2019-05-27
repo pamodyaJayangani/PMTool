95dfcf7383b6cdfdae2f21a0ecc27e1239c98297
Pamodya Jayangani <pamodyajayangani@Pamodyas-Mac.local>
2019-05-26 23:35:30 -0700

    Progressbar

--- PMTool/DetailViewController.swift
+++ PMTool/DetailViewController.swift
@@ -57,17 +57,15 @@ class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDa
         // Do any additional setup after loading the view, typically from a nib.
         
         configureView()
-        print("Zzzz")
-       //============
-//         self.taskView.register(UITableViewCell.self, forCellReuseIdentifier: self.cellName)
+       
         tableView.delegate = self
         tableView.dataSource = self
         
         if let split = splitViewController {
             let controllers = split.viewControllers
-        //    detailViewController = (controllers[controllers.count-1] as! UINavigationController9).topViewController as? DetailViewController
+       
         }
-        //====
+       
     }
 
     var  detailItem: Projects? {
@@ -79,38 +77,7 @@ class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDa
 
 
     @IBOutlet weak var taskView: UITableView!
-    //========
-//    override func viewWillAppear(_ animated: Bool) {
-//        clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
-//        super.viewWillAppear(animated)
-//    }
-//
     
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        if segue.identifier == "showDetail" {
-//            if let indexPath = tableView.indexPathForSelectedRow {
-//                let object = fetchedResultsController.object(at: indexPath)
-//                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-//                controller.detailItem = object
-//                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
-//                controller.navigationItem.leftItemsSupplementBackButton = true
-//            }
-//        }
-//    }
-//
-    
-//     func numberOfSections(in tableView: UITableView) -> Int {
-//      // return fetchedResultsController.sections?.count ?? 0
-//        return arr.count
-//    }
-    
-    ///==========
-    
-    
-    
-    
-    
-
     
     override func prepare(for segue: UIStoryboardSegue, sender: (Any)?)
     {
@@ -123,7 +90,6 @@ class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDa
         if segue.destination is UpdateTaskViewController{
             let index = tableView.indexPathForSelectedRow
             getData(forRowAt: index!)
-            // if segue.identifier == "UpdateTask" {
             let updateController = (segue.destination as? UpdateTaskViewController)
             updateController?.taskName = taskName
             updateController?.tNote = tNote
@@ -131,8 +97,7 @@ class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDa
             updateController?.taskPriority = taskPriority
             updateController?.projectName = projectName
             
-            //print("Update\(arr[indexPath.row])")
-
+           
     }
     }
     
@@ -172,201 +137,8 @@ class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDa
     }
     
     
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        //==
-//        //let sectionInfo = fetchedResultsController.sections![section]
-//        //  return sectionInfo.numberOfObjects
-//        return arr.count
-//        //===
-//    }
-    
-    
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: self.cellName) as! UITableViewCell
-//        //        cell.textLabel?.lineBreakMode = .byWordWrapping
-//        //        cell.textLabel?.numberOfLines = 3
-//        cell.textLabel?.text = arr[indexPath.row].taskName!
-//
-//        return cell
-//    }
- 
-    // Override to support editing the table view.
-//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == .delete {
-////            tableView.deleteRows(at: [indexPath], with: .fade)
-//            arr.remove(at: indexPath.row)
-//            tableView.reloadData()
-//
-//            // Delete the row from the data source
-//            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
-//
-//            //We need to create a context from this container
-//            let managedContext = appDelegate.persistentContainer.viewContext
-//
-//
-////            getData(forRowAt: indexPath)
-//
-//            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Tasks")
-//            fetchRequest.predicate = NSPredicate(format: "taskName = %@", taskName)
-//            fetchRequest.predicate = NSPredicate(format: "projectName = %@", projectName)
-//
-//
-//            do
-//            {
-//                let tasks = try managedContext.fetch(fetchRequest)
-//
-//                let objectToDelete = tasks[indexPath.row] as! NSManagedObject
-//                managedContext.delete(objectToDelete)
-//
-//                do{
-//                    try managedContext.save()
-//                }
-//                catch
-//                {
-//                    print(error)
-//                }
-//
-//            }
-//            catch
-//            {
-//                print(error)
-//            }
-//        } else if editingStyle == .insert {
-//            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-//        }
-//    }
-    
-//    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-//        return true
-//    }
-//
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        ///==
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-//        let event = fetchedResultsController.object(at: indexPath)
-//        configureCell(cell, withEvent: event)
-//        return cell
-//        //===
-//
-//    }
-//    //=======
-//     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-//        // Return false if you do not want the specified item to be editable.
-//        return true
-//    }
-//
-//     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == .delete {
-//            let context = fetchedResultsController.managedObjectContext
-//            context.delete(fetchedResultsController.object(at: indexPath))
-//
-//            do {
-//                try context.save()
-//            } catch {
-//                // Replace this implementation with code to handle the error appropriately.
-//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//                let nserror = error as NSError
-//                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-//            }
-//        }
-//            }
-//
-//
-//    func configureCell(_ cell: UITableViewCell, withEvent event: Tasks) {
-//        cell.textLabel!.text = "event.taskName!.description"
-//    }
-//
-//    // MARK: - Fetched results controller
-//
-//    var fetchedResultsController: NSFetchedResultsController<Tasks> {
-//        if _fetchedResultsController != nil {
-//            return _fetchedResultsController!
-//        }
-//
-////        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-////        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-////
-////        cell.textLabel?.text = "DEF GHI POU ERT TYO ABC"
-////
-////        return cell
-////    }
-//
-//        let fetchRequest: NSFetchRequest<Tasks> = Tasks.fetchRequest()
-//
-//        // Set the batch size to a suitable number.
-//        fetchRequest.fetchBatchSize = 20
-//
-//        // Edit the sort key as appropriate.
-//        let sortDescriptor = NSSortDescriptor(key: "taskName", ascending: false)
-//
-//        fetchRequest.sortDescriptors = [sortDescriptor]
-//
-//        // Edit the section name key path and cache name if appropriate.
-//        // nil for section name key path means "no sections".
-//        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext!, sectionNameKeyPath: nil, cacheName: "Master")
-//        aFetchedResultsController.delegate = self
-//        _fetchedResultsController = aFetchedResultsController
-//
-//        do {
-//            try _fetchedResultsController!.performFetch()
-//        } catch {
-//            // Replace this implementation with code to handle the error appropriately.
-//            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//            let nserror = error as NSError
-//            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-//        }
-//
-//        return _fetchedResultsController!
-//    }
-//
-//
-//    var _fetchedResultsController: NSFetchedResultsController<Tasks>? = nil
-//
-//    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-//        taskView.beginUpdates()
-//    }
-//
-//    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
-//        switch type {
-//        case .insert:
-//            taskView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
-//        case .delete:
-//            taskView.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
-//        default:
-//            return
-//        }
-//    }
-//
-//    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-//        switch type {
-//        case .insert:
-//            taskView.insertRows(at: [newIndexPath!], with: .fade)
-//        case .delete:
-//            taskView.deleteRows(at: [indexPath!], with: .fade)
-//        case .update:
-//            configureCell(taskView.cellForRow(at: indexPath!)!, withEvent: anObject as! Tasks)
-//        case .move:
-//            configureCell(taskView.cellForRow(at: indexPath!)!, withEvent: anObject as! Tasks)
-//            taskView.moveRow(at: indexPath!, to: newIndexPath!)
-//        }
-//    }
-//
-//    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-//        taskView.endUpdates()
-//    }
-//
-//    //=====
-    
+
+
     func numberOfSections(in tableView: UITableView) -> Int {
         return self.fetchedResultsController.sections?.count ?? 0
         //        return 1
